11f170da0b0f668aabea6957b773810e1d56744b
MarkDaDave <mdmartin983@gmail.com>
2024-06-22 22:17:59 +0800

    updated

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImg1 = "images/dice" + randomNumber1 + ".png";
+document.querySelector(".img1").setAttribute("src", randomDiceImg1);
 
-var randomDiceImg = "images/dice" + randomNumber1 + ".png";
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImg2 = "images/dice" + randomNumber2 + ".png";
+document.querySelector(".img2").setAttribute("src", randomDiceImg2);
 
-document.querySelector(".img1").setAttribute("src", randomDiceImg);
-
-document.querySelector(".img2").setAttribute("src", randomDiceImg);
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 1 Wins";
+} else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins";
+} else {
+  document.querySelector("h1").innerHTML = "Draw";
+}
